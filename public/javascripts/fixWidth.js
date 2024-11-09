2ca585c755ad085abb6ab70989302967b2c5dc7d
Mollive <mollive91@gmail.com>
2024-11-10 03:18:05 +0900

    반응형 제거 css,js 갱신

--- public/javascripts/fixWidth.js
+++ public/javascripts/fixWidth.js
@@ -1,6 +1,5 @@
 //반응형 제거
 $(document).ready(function () {
-  //공용
   $("body").css('min-width', '1200px');
 
   //로그인
@@ -13,20 +12,13 @@ $(document).ready(function () {
   $("body #loginForm").css('width', '400px');  
 
   //메인보드
-  $("body").css('padding-top', '0');
-  $("body nav").removeClass('fixed-top navbar-expand-md');
   $("body nav button:nth-child(1)").css('display', 'none');
   $("body nav #navbarNavDropdown").css('display', '');
-  $("body nav a.navbar-right-none").css('margin-left', 'auto');
+  $("body nav #aNotice").css('margin-left', 'auto');
 
   $("a.navbar-right-none").css('display', '');
   $("button.navbar-right-none").css('display', '');
 
   $("body #sidebar-container").removeClass('d-none');
-  $("body #sidebar-container").css('min-height', '100vh');
-
-  $("body #body-row").removeClass('row');
-  $("body #body-row").css('flex-wrap', 'nowrap');
-  $("body #body-row").css('display', 'flex');  
-  $("body #body-row ul").removeClass('sticky-top sticky-offset');
+  $("body #footer").css('min-width', '1200px');
 });
