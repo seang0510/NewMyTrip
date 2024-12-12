8e1e5ce8200fbe18ed6f771acb49377b2869a613
Mollive <mollive91@gmail.com>
2024-12-13 03:27:01 +0900

    로그인 화면 변경

--- public/javascripts/fixWidth.js
+++ public/javascripts/fixWidth.js
@@ -2,15 +2,6 @@
 $(document).ready(function () {
   $("body").css('min-width', '1200px');
 
-  //로그인
-  $("body #loginRow").removeClass('g-lg-5');
-  $("body #loginRow").css('flex-wrap', 'nowrap');
-  $("body #loginImage").removeClass('col-lg-7');
-  $("body #loginImage").css('width', '631px');
-  $("body #loginImage img").css('width', '100%');
-  $("body #loginForm").removeClass('col-md-10 col-lg-5');
-  $("body #loginForm").css('width', '400px');  
-
   //메인보드
   $("body nav button:nth-child(1)").css('display', 'none');
   $("body nav #navbarNavDropdown").css('display', '');
